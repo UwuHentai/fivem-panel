6b4d22d768aeb4923d045a4e776c7a386c297cdd
Uwu Hentai <hentaiuwu2000@gmail.com>
2022-05-24 19:27:21 +0300

    Update vRPUsers.php

--- app/Http/Controllers/vRPUsers.php
+++ app/Http/Controllers/vRPUsers.php
@@ -20,8 +20,8 @@ class vRPUsers extends Controller
             $id = $request->input('id');
             $reason = $request->input('reason');
             $ban = DB::table('vrp_users')->where('id', $id)->update(['banned' => 1]);
-            // $ban_reason = DB::table('vrp_users')->where('id', $id)->update(['banreason' => $reason . " [PANEL]"]);
-            // $admin = DB::table('vrp_users')->where('id', $id)->update(['banadmin' => Auth::user()->name]);
+            $ban_reason = DB::table('vrp_users')->where('id', $id)->update(['banreason' => $reason . " [PANEL]"]);
+            $admin = DB::table('vrp_users')->where('id', $id)->update(['banadmin' => Auth::user()->name]);
             return redirect()->back();
         } else {
             return view('panel/home');
@@ -35,7 +35,7 @@ class vRPUsers extends Controller
             $reason = $request->input('reason');
             $unban = DB::table('vrp_users')->where('id', $id)->update(['banned' => 0]);
             // $unban_reason = DB::table('vrp_users')->where('id', $id)->update(['banreason' => $reason . " [PANEL]"]);
-            // $admin = DB::table('vrp_users')->where('id', $id)->update(['banadmin' => Auth::user()->name]);
+            $admin = DB::table('vrp_users')->where('id', $id)->update(['banadmin' => Auth::user()->name]);
             return redirect()->back();
         } else {
             return view('panel/home');
