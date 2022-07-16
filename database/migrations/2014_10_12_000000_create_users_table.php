39e295e5501978951246330e8fafd969bea83fc8
UwuHentai <hentaiuwu2000@gmail.com>
2022-07-16 06:57:16 -0700

    0.0.1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->integer('administrator')->default(0);
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {return view('welcome');});
-Route::get('panel/home', function () {return view('panel/home');})->name('panel.home');
+Route::get('/', function () {
+    return view('home');
+});
 
 Auth::routes();
-
-Route::get('admin/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('admin.dashboard');
-
-Route::get('vrp_users/ban', [App\Http\Controllers\vRPUsers::class, 'vRPUsers_ban'])->name('ban.user');
-Route::get('vrp_users/unban', [App\Http\Controllers\vRPUsers::class, 'vRPUsers_unban'])->name('unban.user');
-Route::get('vrp_users/whitelist', [App\Http\Controllers\vRPUsers::class, 'vRPUsers_whitelist'])->name('whitelist.user');
-Route::get('vrp_users/unwhitelist', [App\Http\Controllers\vRPUsers::class, 'vRPUsers_unwhitelist'])->name('unwhitelist.user');
-Route::get('vrp_users_money/addmoneywallet', [App\Http\Controllers\vRPUsersMoney::class, 'vRPUsersMoney_addwallet'])->name('addmoneywallet.user');
-Route::get('vrp_users_money/removemoneywallet', [App\Http\Controllers\vRPUsersMoney::class, 'vRPUsersMoney_removewallet'])->name('removemoneywallet.user');
-Route::get('vrp_users_money/addmoneybank', [App\Http\Controllers\vRPUsersMoney::class, 'vRPUsersMoney_addbank'])->name('addmoneybank.user');
-Route::get('vrp_users_money/removemoneybank', [App\Http\Controllers\vRPUsersMoney::class, 'vRPUsersMoney_removebank'])->name('removemoneybank.user');
